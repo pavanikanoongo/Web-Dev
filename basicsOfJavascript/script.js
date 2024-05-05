f19bc3c613043f76c40d3ce93fd1046563131b3b
Pavani <pavanikanoongo15@gmail.com>
2024-05-05 17:07:15 +0530

    Added a basic javascript programs file

---
+++ basicsOfJavascript/script.js
@@ -0,0 +1,94 @@
+// // //Program using arithmetic Operators
+let a = 20;
+let b = 2;
+let op = prompt("Enter operator: ");
+switch (op) {
+    case '+':
+        console.log("result=", a + b);
+        break;
+    case '-':
+        console.log("result=", a - b);
+        break;
+    case '*':
+        console.log("result=", a * b);
+        break;
+    case '/':
+        console.log("result=", a / b);
+        break;
+    case '++':
+        a++;
+        b++;
+        console.log("a=", a);
+        console.log("b=", b);
+        break;
+    case '--':
+        a--;
+        b--;
+        console.log("a=", a);
+        console.log("b=", b);
+        break;
+    case '**':
+        console.log("result=", a**b);
+        break;
+
+    default:
+        console.log("Invalid operator");
+}
+
+
+
+// // //Program for average marks 
+let e=92
+let h=95
+let m=96
+let s=97
+let so=91
+console.log("english= ",e);
+console.log("hindi= ",h);
+console.log("maths= ",m);
+console.log("science= ",s);
+console.log("social= ",so);
+let sum=(e+h+m+s+so);
+console.log("Sum =",sum)
+let avg=sum/5;
+console.log("Average= ",avg);
+
+
+// //Program to check string is palindrome
+function isPalindrome(str) {
+    let j = str.length - 1;
+    for (i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[j]) {
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
+result = isPalindrome("racecar");
+if (result == true) {
+    console.log("Palindrome ")
+}
+else {
+    console.log("Not a palindrome");
+}
+
+
+
+// //Program for string in alphabetical order
+let pos;
+let newStr;
+let alph=(str)=>{
+    var arr=str.split("");
+    newStr=arr.sort().join("");
+    console.log(newStr);
+}
+alph("javascript");
+
+//Function for Average marks
+let average=(n1,n2,n3,n4,n5)=>{
+    sum=n1+n2+n3+n4+n5;
+    avg=sum/5;
+    console.log("average =",avg);
+}
+average(95,92,96,94,91);
