b9dda5b0d6bcff78807cbd99dc4542615a789574
Pavani <pavanikanoongo15@gmail.com>
2024-07-07 13:23:19 +0530

    Navigation toggler using javascript

---
+++ toggler/script.js
@@ -0,0 +1,5 @@
+const toggler = document.getElementById('toggler');
+const navLinks = document.getElementById('nav-links');
+toggler.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
